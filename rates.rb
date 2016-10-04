b9fa5bc829ecdc1ff65a4253dfe8e688f07df6c5
MonKoDev Technologies <gmalugani@gmail.com>
2016-10-04 19:33:56 -0300

    Cambia monedas a peso Uruguayo

--- rates.rb
+++ rates.rb
@@ -6,5 +6,7 @@ Money.default_bank = Money::Bank::GoogleCurrency.new
 Money.use_i18n = false
 
 puts "Las tasas hoy son: "
-puts "   Dolar: #{Money.new(100, :USD).exchange_to(:COP).format}"
-puts "   Euro: #{Money.new(100, :EUR).exchange_to(:COP).format}"
+puts "   Dolar: #{Money.new(100, :USD).exchange_to(:UYU).format}"
+puts "   Euro: #{Money.new(100, :EUR).exchange_to(:UYU).format}"
+puts "   Peso Mexicano: #{Money.new(100, :MXN).exchange_to(:UYU).format}"
+puts "   Peso Argentino: #{Money.new(100, :ARS).exchange_to(:UYU).format}"
