d54ee9eaf8dce18f3ebf49c34d0cb3aed8cb4765
MonKoDev Technologies <gmalugani@gmail.com>
2016-11-22 20:39:42 -0300

    Actualiza task.rb

--- tasks.rb
+++ tasks.rb
@@ -1,24 +1,34 @@
 def load_tasks(file)
+	
+	hashi=[]
 	if File.exist?(file)==false
-		hash=Hash[{}]
+		
 	else
 			array=IO.readlines(file)
 			if array.empty?
-				hash=Hash[{}]
-			else
-				ha = File.foreach(file).map { |line| line.split(',') }
-				#hash = ha[0][0]
-				for i in (0..ha.size-1)
-					hash= {:id => ha[i][0], :name => ha[i][1], :done => ha[i][2] }
-				end
-				#hash=ha[0][0]
-				#h=array.split(/\s*,\s*/)
-				#hash= h
 				
-  			end		
+			else
+				prueba = Array.new(2, Hash.new)
+				# prueba crea => [{}, {}]
+
+				a=["id: ","name: ","done: "]
+				
+				ha = File.foreach(file).map { |line| line.split(',') }
+				#a.map! {|x,y,z| x +  }
+				#hashi=a+ha
+				#hash = ha[0][0]
+				hashi=a.merge! ha
+				for i in (0..ha.size-1)						
+					#hashi[i]= { "id:".to_s => ha[i][0].to_i, "name:".to_s => ha[i][1], "done:".to_s => (ha[i][2].chomp).to_s.eql?('true') ? true : false }						
+					#hashi[i]= { ha[i][0].to_i, ha[i][1],  (ha[i][2].chomp).to_s.eql?('true') ? true : false }
+					a.merge! 
+					#hashi=a
+				end
+			end		
 	end
-	puts hash
+	puts hashi
 end
+
 load_tasks('test.txt')
 
 
